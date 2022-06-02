c465f72c9e935fe480564b66953d193b865ae07e
gabriel19971029 <clementeaquipresente@yahoo.com>
2022-06-02 22:38:47 +0200

    first commit

---
+++ src/token_assets/src/components/Faucet.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { canisterId, createActor } from "../../../declarations/token";
+import { AuthClient } from "@dfinity/auth-client";
+
+function Faucet(props) {
+
+  const [isDisabled, setDisable] = useState(false);
+  const [buttonText, setText] = useState("Gimme gimme");
+
+  async function handleClick(event) {
+    setDisable(true);
+
+    const authClient = await AuthClient.create();
+    const identity = await authClient.getIdentiy();
+
+    const authenticatedCanister = createActor(canisterId, {
+      agentOptions: {
+        identity, 
+      },
+    });
+
+    const result = await authenticatedCanister.payOut();
+    setText(result);
+  }
+
+  return (
+    <div className="blue window">
+      <h2>
+        <span role="img" aria-label="tap emoji">
+          🚰
+        </span>
+        Faucet
+      </h2>
+      <label>Get your free GABO tokens here! Claim 10,000 GABO tokens to your {props.userPrincipal}.</label>
+      <p className="trade-buttons">
+        <button id="btn-payout" onClick={handleClick} disabled={isDisabled}>
+          {buttonText}
+        </button>
+      </p>
+    </div>
+  );
+}
+
+export default Faucet;
